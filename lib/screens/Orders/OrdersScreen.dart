73d88bfb743653a094fe710549fd676bff5199ad
Vaibhav77e <chandrashekharvaibhav@gmail.com>
2024-03-05 18:37:45 +0530

    created orderscreen

--- lib/screens/Orders/OrdersScreen.dart
+++ lib/screens/Orders/OrdersScreen.dart
@@ -1,3 +1,4 @@
+import 'package:bar_management_system/utils/MediaqueryHelper.dart';
 import 'package:flutter/material.dart';
 
 class OrdersScreen extends StatelessWidget {
@@ -5,8 +6,19 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryHelper mediaQueryHelper =  MediaQueryHelper(context);
     return Container(
-      child: Text('Orders page'),
+      child:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text('Invoice'),
+        SizedBox(height: mediaQueryHelper.getHeightPercentage(10),),
+        Row(
+          children: [
+            Text('From')
+          ],
+        )
+      ],)
     );
   }
 }
